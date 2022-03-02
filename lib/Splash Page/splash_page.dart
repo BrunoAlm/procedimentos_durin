581ae02c30ecefef0022b4a7f29f8cd047c678c6
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2022-03-02 17:46:38 -0300

    adicionado assets e splashpage

--- lib/Splash Page/splash_page.dart
+++ lib/Splash Page/splash_page.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:procedimentos_durin/Home%20Page/home_page.dart';
+import 'package:splashscreen/splashscreen.dart';
 
-class SplashPage extends StatelessWidget {
-  const SplashPage({ Key? key }) : super(key: key);
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
 
   @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return _introScreen();
   }
+}
+
+Widget _introScreen() {
+  return Stack(
+    children: <Widget>[
+      SplashScreen(
+        seconds: 5,
+        gradientBackground: const LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          colors: [Color(0xffED213A), Color(0xff93291E)],
+        ),
+        navigateAfterSeconds: const HomePage(),
+        loaderColor: Colors.transparent,
+      ),
+      Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/logo.png"),
+            fit: BoxFit.none,
+          ),
+        ),
+      ),
+    ],
+  );
 }
