cb0fc36397669a9736ea46c52b437412ab1698c8
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2022-03-28 15:26:03 -0300

    hasura connext

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,12 +1,21 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:hasura_connect/hasura_connect.dart';
 import 'package:procedimentos_durin/app/modules/auth/auth_module.dart';
-// import 'package:procedimentos_durin/app/modules/chamados/chamados_module.dart';
 import 'package:procedimentos_durin/app/modules/home/home_module.dart';
 import 'package:procedimentos_durin/app/modules/splash/splash_page.dart';
 
 class AppModule extends Module {
   @override
-  final List<Bind> binds = [];
+  final List<Bind> binds = [
+    Bind(
+      (i) => HasuraConnect("https://procedimentos-durin.hasura.app/v1/graphql",
+          headers: {
+            'x-hasura-admin-secret':
+                'UQWt54LcSNSqtlkJMXSeMtKbb8sIi4absKtFs1dqJphBigKnUIKAkQhJ32nbi7PB',
+            'content-type': 'application/json',
+          }),
+    )
+  ];
 
   @override
   final List<ModularRoute> routes = [
